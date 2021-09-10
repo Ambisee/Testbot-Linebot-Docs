f961f042d432aa6b1c2d3dd4b6229280784fdda0
Ambisee <celvin.weilington@gmail.com>
2021-09-10 15:31:03 -0700

    Increase the size limit of some columns on the db models

--- webpage_api/utils/models.py
+++ webpage_api/utils/models.py
@@ -12,12 +12,12 @@ class Function(db.Model):
     name = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.String(500), nullable=False)
     syntax = db.Column(db.String(100), nullable=False)
-    user_chat = db.Column(db.String(100), nullable=False)
-    bot_chat = db.Column(db.String(100), nullable=False)
+    user_chat = db.Column(db.String(500), nullable=False)
+    bot_chat = db.Column(db.String(500), nullable=False)
 
 
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
